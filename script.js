a225afda1fb7bb4f4da5d358a312192b4d6ef41b
MUHAMMAD DYAZ HAIRULLAH <muhammad.dyaz203@sma.belajar.id>
2026-02-17 12:15:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,445 @@
+// DATABASE VIDEO (Diambil dari bot_telegram.py)
+const VIDEO_DATABASE = {
+    "Pilot 01": {
+        "title": "Ninjago Pilot: Way of The Ninja",
+        "type": "pilot",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/83"],
+            2: ["https://t.me/ninjagosubtitleindo/84"],
+            3: ["https://t.me/ninjagosubtitleindo/85"],
+            4: ["https://t.me/ninjagosubtitleindo/86"],
+        }
+    },
+    "Season 1": {
+        "title": "Ninjago Season 1: Rise of the Snakes",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/93"],
+            2: ["https://t.me/ninjagosubtitleindo/94"],
+            3: ["https://t.me/ninjagosubtitleindo/95"],
+            4: ["https://t.me/ninjagosubtitleindo/96"],
+            5: ["https://t.me/ninjagosubtitleindo/97"],
+            6: ["https://t.me/ninjagosubtitleindo/99"],
+            7: ["https://t.me/ninjagosubtitleindo/100"],
+            8: ["https://t.me/ninjagosubtitleindo/101"],
+            9: ["https://t.me/ninjagosubtitleindo/102"],
+            10: ["https://t.me/ninjagosubtitleindo/103"],
+            11: ["https://t.me/ninjagosubtitleindo/104"],
+            12: ["https://t.me/ninjagosubtitleindo/105"],
+            13: ["https://t.me/ninjagosubtitleindo/106"],
+        }
+    },
+    "Season 2": {
+        "title": "Ninjago Season 2: Legacy of the Green Ninja",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/110"],
+            2: ["https://t.me/ninjagosubtitleindo/111"],
+            3: ["https://t.me/ninjagosubtitleindo/112"],
+            4: ["https://t.me/ninjagosubtitleindo/113"],
+            5: ["https://t.me/ninjagosubtitleindo/115"],
+            6: ["https://t.me/ninjagosubtitleindo/116"],
+            7: ["https://t.me/ninjagosubtitleindo/117"],
+            8: ["https://t.me/ninjagosubtitleindo/118"],
+            9: ["https://t.me/ninjagosubtitleindo/119"],
+            10: ["https://t.me/ninjagosubtitleindo/120"],
+            11: ["https://t.me/ninjagosubtitleindo/121"],
+            12: ["https://t.me/ninjagosubtitleindo/122"],
+            13: ["https://t.me/ninjagosubtitleindo/123"],
+        }
+    },
+    "Season 3": {
+        "title": "Ninjago Season 3: Rebooted",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/130"],
+            2: ["https://t.me/ninjagosubtitleindo/131"],
+            3: ["https://t.me/ninjagosubtitleindo/132"],
+            4: ["https://t.me/ninjagosubtitleindo/133"],
+            5: ["https://t.me/ninjagosubtitleindo/134"],
+            6: ["https://t.me/ninjagosubtitleindo/135"],
+            7: ["https://t.me/ninjagosubtitleindo/136"],
+            8: ["https://t.me/ninjagosubtitleindo/137"],
+        }
+    },
+    "Season 4": {
+        "title": "Ninjago Season 4: Tournament of Elements",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/141"],
+            2: ["https://t.me/ninjagosubtitleindo/143"],
+            3: ["https://t.me/ninjagosubtitleindo/144"],
+            4: ["https://t.me/ninjagosubtitleindo/145"],
+            5: ["https://t.me/ninjagosubtitleindo/146"],
+            6: ["https://t.me/ninjagosubtitleindo/147"],
+            7: ["https://t.me/ninjagosubtitleindo/148"],
+            8: ["https://t.me/ninjagosubtitleindo/149"],
+            9: ["https://t.me/ninjagosubtitleindo/150"],
+            10: ["https://t.me/ninjagosubtitleindo/151"],
+        }
+    },
+    "Season 5": {
+        "title": "Ninjago Season 5: Possession",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/165"],
+            2: ["https://t.me/ninjagosubtitleindo/167"],
+            3: ["https://t.me/ninjagosubtitleindo/189"],
+            4: ["https://t.me/ninjagosubtitleindo/190"],
+            5: ["https://t.me/ninjagosubtitleindo/191"],
+            6: ["https://t.me/ninjagosubtitleindo/192"],
+            7: ["https://t.me/ninjagosubtitleindo/193"],
+            8: ["https://t.me/ninjagosubtitleindo/194"],
+            9: ["https://t.me/ninjagosubtitleindo/195"],
+            10: ["https://t.me/ninjagosubtitleindo/196"],
+        }
+    },
+    "Season 6": {
+        "title": "Ninjago Season 6: Skybound",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/185"],
+            2: ["https://t.me/ninjagosubtitleindo/186"],
+            3: ["https://t.me/ninjagosubtitleindo/187"],
+            4: ["https://t.me/ninjagosubtitleindo/188"],
+            5: ["https://t.me/ninjagosubtitleindo/189"],
+            6: ["https://t.me/ninjagosubtitleindo/190"],
+            7: ["https://t.me/ninjagosubtitleindo/191"],
+            8: ["https://t.me/ninjagosubtitleindo/192"],
+            9: ["https://t.me/ninjagosubtitleindo/193"],
+            10: ["https://t.me/ninjagosubtitleindo/194"],
+        }
+    },
+    "Season 7": {
+        "title": "Ninjago Season 7: The Hands of Time",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/214"],
+            2: ["https://t.me/ninjagosubtitleindo/215"],
+            3: ["https://t.me/ninjagosubtitleindo/216"],
+            4: ["https://t.me/ninjagosubtitleindo/217"],
+            5: ["https://t.me/ninjagosubtitleindo/218"],
+            6: ["https://t.me/ninjagosubtitleindo/219"],
+            7: ["https://t.me/ninjagosubtitleindo/220"],
+            8: ["https://t.me/ninjagosubtitleindo/221"],
+            9: ["https://t.me/ninjagosubtitleindo/222"],
+            10: ["https://t.me/ninjagosubtitleindo/223"],
+        }
+    },
+    "Season 8": {
+        "title": "Ninjago Season 8: Sons of Garmadon",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/252"],
+            2: ["https://t.me/ninjagosubtitleindo/253"],
+            3: ["https://t.me/ninjagosubtitleindo/254"],
+            4: ["https://t.me/ninjagosubtitleindo/255"],
+            5: ["https://t.me/ninjagosubtitleindo/256"],
+            6: ["https://t.me/ninjagosubtitleindo/257"],
+            7: ["https://t.me/ninjagosubtitleindo/259"],
+            8: ["https://t.me/ninjagosubtitleindo/260"],
+            9: ["https://t.me/ninjagosubtitleindo/261"],
+            10: ["https://t.me/ninjagosubtitleindo/262"],
+        }
+    },
+    "Season 9": {
+        "title": "Ninjago Season 9: Hunted",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/267"],
+            2: ["https://t.me/ninjagosubtitleindo/268"],
+            3: ["https://t.me/ninjagosubtitleindo/269"],
+            4: ["https://t.me/ninjagosubtitleindo/270"],
+            5: ["https://t.me/ninjagosubtitleindo/271"],
+            6: ["https://t.me/ninjagosubtitleindo/272"],
+            7: ["https://t.me/ninjagosubtitleindo/273"],
+            8: ["https://t.me/ninjagosubtitleindo/274"],
+            9: ["https://t.me/ninjagosubtitleindo/275"],
+            10: ["https://t.me/ninjagosubtitleindo/276"],
+        }
+    },
+    "Season 10": {
+        "title": "Ninjago Season 10: March of the Oni",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/279"],
+            2: ["https://t.me/ninjagosubtitleindo/280"],
+            3: ["https://t.me/ninjagosubtitleindo/281"],
+            4: ["https://t.me/ninjagosubtitleindo/282"],
+            5: ["https://t.me/ninjagosubtitleindo/283"],
+            6: ["https://t.me/ninjagosubtitleindo/284"],
+            7: ["https://t.me/ninjagosubtitleindo/285"],
+            8: ["https://t.me/ninjagosubtitleindo/286"],
+            9: ["https://t.me/ninjagosubtitleindo/287"],
+            10: ["https://t.me/ninjagosubtitleindo/288"],
+        }
+    },
+    "Season 11": {
+        "title": "Ninjago Season 11: Secrets of the Forbidden Spinjitzu",
+        "type": "playlist_multi",
+        "playlists": [
+            {"name": "🔥 Fire Chapter (Eps 1-15)", "url": "https://youtube.com/playlist?list=PL-5C6K7ocUWHYXacyA7oDNQiJIZv2jtCh&si=dXZWaNz738ykWHOk"},
+            {"name": "❄️ Ice Chapter (Eps 16-30)", "url": "https://youtube.com/playlist?list=PLrML7d4aiT-zWBb73Nzlwxs88mE2Y6u19&si=Lnq9KKB_ODqNymgL"}
+        ]
+    },
+    "Season 12": {
+        "title": "Ninjago Season 12: Prime Empire",
+        "type": "playlist",
+        "url": "https://youtube.com/playlist?list=PL-5C6K7ocUWFsYFeCSkGW5RpQ1YQHi9IR&si=Nk2m__l-ooiJ53gy"
+    },
+    "Season 13": {
+        "title": "Ninjago Season 13: Master of the Mountain",
+        "type": "playlist",
+        "url": "https://youtube.com/playlist?list=PL-5C6K7ocUWE9euVJpfFIvUXfkX-MgQ_m&si=BlEMuxPpsWPqxl5z"
+    },
+    "The Island": {
+        "title": "Ninjago Season 14: The Island",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/297"],
+            2: ["https://t.me/ninjagosubtitleindo/298"],
+            3: ["https://t.me/ninjagosubtitleindo/299"],
+            4: ["https://t.me/ninjagosubtitleindo/300"],
+        }
+    },
+    "Seabound": {
+        "title": "Ninjago Season 15: Seabound",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/303"],
+            2: ["https://t.me/ninjagosubtitleindo/304"],
+            3: ["https://t.me/ninjagosubtitleindo/305"],
+            4: ["https://t.me/ninjagosubtitleindo/306"],
+            5: ["https://t.me/ninjagosubtitleindo/312"],
+            6: ["https://t.me/ninjagosubtitleindo/313"],
+            7: ["https://t.me/ninjagosubtitleindo/314"],
+            8: ["https://t.me/ninjagosubtitleindo/315"],
+            9: ["https://t.me/ninjagosubtitleindo/316"],
+            10: ["https://t.me/ninjagosubtitleindo/317"],
+            11: ["https://t.me/ninjagosubtitleindo/319"],
+            12: ["https://t.me/ninjagosubtitleindo/321"],
+            13: ["https://t.me/ninjagosubtitleindo/322"],
+            14: ["https://t.me/ninjagosubtitleindo/324"],
+            15: ["https://t.me/ninjagosubtitleindo/325"],
+            16: ["https://t.me/ninjagosubtitleindo/326"],
+        }
+    },
+    "Crystalized": {
+        "title": "Ninjago Season 16: Crystalized",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/ninjagosubtitleindo/347"],
+            2: ["https://t.me/ninjagosubtitleindo/348"],
+            3: ["https://t.me/ninjagosubtitleindo/349"],
+            4: ["https://t.me/ninjagosubtitleindo/350"],
+            5: ["https://t.me/ninjagosubtitleindo/351"],
+            6: ["https://t.me/ninjagosubtitleindo/352"],
+            7: ["https://t.me/ninjagosubtitleindo/353"],
+            8: ["https://t.me/ninjagosubtitleindo/354"],
+            9: ["https://t.me/ninjagosubtitleindo/355"],
+            10: ["https://t.me/ninjagosubtitleindo/356"],
+            11: ["https://t.me/ninjagosubtitleindo/358"],
+            12: ["https://t.me/ninjagosubtitleindo/360"],
+            13: ["https://t.me/ninjagosubtitleindo/361"],
+            14: ["https://t.me/ninjagosubtitleindo/362"],
+            15: ["https://t.me/ninjagosubtitleindo/363"],
+            16: ["https://t.me/ninjagosubtitleindo/364"],
+            17: ["https://t.me/ninjagosubtitleindo/365"],
+            18: ["https://t.me/ninjagosubtitleindo/366"],
+            19: ["https://t.me/ninjagosubtitleindo/367"],
+            20: ["https://t.me/ninjagosubtitleindo/368"],
+            21: ["https://t.me/ninjagosubtitleindo/369"],
+            22: ["https://t.me/ninjagosubtitleindo/370"],
+            23: ["https://t.me/ninjagosubtitleindo/371"],
+            24: ["https://t.me/ninjagosubtitleindo/372"],
+            25: ["https://t.me/ninjagosubtitleindo/373"],
+            26: ["https://t.me/ninjagosubtitleindo/374"],
+            27: ["https://t.me/ninjagosubtitleindo/375"],
+            28: ["https://t.me/ninjagosubtitleindo/376"],
+            29: ["https://t.me/ninjagosubtitleindo/377"],
+            30: ["https://t.me/ninjagosubtitleindo/378"],
+        }
+    },
+    "Dragon Rising 1": {
+        "title": "Ninjago Season 17: Dragon Rising #PART1",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/dragonrisingsubindo/6"],
+            2: ["https://t.me/dragonrisingsubindo/7"],
+            3: ["https://t.me/dragonrisingsubindo/8"],
+            4: ["https://t.me/dragonrisingsubindo/9"],
+            5: ["https://t.me/dragonrisingsubindo/10"],
+            6: ["https://t.me/dragonrisingsubindo/11"],
+            7: ["https://t.me/dragonrisingsubindo/12"],
+            8: ["https://t.me/dragonrisingsubindo/13"],
+            9: ["https://t.me/dragonrisingsubindo/14"],
+            10: ["https://t.me/dragonrisingsubindo/15"],
+            11: ["https://t.me/dragonrisingsubindo/16"],
+            12: ["https://t.me/dragonrisingsubindo/17"],
+            13: ["https://t.me/dragonrisingsubindo/18"],
+            14: ["https://t.me/dragonrisingsubindo/19"],
+            15: ["https://t.me/dragonrisingsubindo/20"],
+        }
+    },
+    "Dragon Rising 2": {
+        "title": "Ninjago Season 18: Dragon Rising #PART2",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/dragonrisingsubindo/28"],
+            2: ["https://t.me/dragonrisingsubindo/29"],
+            3: ["https://t.me/dragonrisingsubindo/30"],
+            4: ["https://t.me/dragonrisingsubindo/31"],
+            5: ["https://t.me/dragonrisingsubindo/32"],
+            6: ["https://t.me/dragonrisingsubindo/33"],
+            7: ["https://t.me/dragonrisingsubindo/34"],
+            8: ["https://t.me/dragonrisingsubindo/35"],
+            9: ["https://t.me/dragonrisingsubindo/36"],
+            10: ["https://t.me/dragonrisingsubindo/37"],
+            11: ["https://t.me/dragonrisingsubindo/38"],
+            12: ["https://t.me/dragonrisingsubindo/39"],
+            13: ["https://t.me/dragonrisingsubindo/40"],
+            14: ["https://t.me/dragonrisingsubindo/41"],
+            15: ["https://t.me/dragonrisingsubindo/42"],
+            16: ["https://t.me/dragonrisingsubindo/43"],
+            17: ["https://t.me/dragonrisingsubindo/44"],
+            18: ["https://t.me/dragonrisingsubindo/45"],
+            19: ["https://t.me/dragonrisingsubindo/46"],
+            20: ["https://t.me/dragonrisingsubindo/47"],
+        }
+    },
+    "Dragon Rising 3": {
+        "title": "Ninjago Season 19: Dragon Rising #PART3",
+        "type": "season",
+        "episodes": {
+            1: ["https://t.me/dragonrisingsubindo/50"],
+            2: ["https://t.me/dragonrisingsubindo/51"],
+            3: ["https://t.me/dragonrisingsubindo/52"],
+            4: ["https://t.me/dragonrisingsubindo/53"],
+            5: ["https://t.me/dragonrisingsubindo/54"],
+            6: ["https://t.me/dragonrisingsubindo/55"],
+            7: ["https://t.me/dragonrisingsubindo/56"],
+            8: ["https://t.me/dragonrisingsubindo/57"],
+            9: ["https://t.me/dragonrisingsubindo/58"],
+            10: ["https://t.me/dragonrisingsubindo/59"],
+            11: ["https://t.me/dragonrisingsubindo/60"],
+            12: ["https://t.me/dragonrisingsubindo/61"],
+            13: ["https://t.me/dragonrisingsubindo/62"],
+            14: ["https://t.me/dragonrisingsubindo/63"],
+            15: ["https://t.me/dragonrisingsubindo/64"],
+            16: ["https://t.me/dragonrisingsubindo/65"],
+            17: ["https://t.me/dragonrisingsubindo/66"],
+            18: ["https://t.me/dragonrisingsubindo/67"],
+            19: ["https://t.me/dragonrisingsubindo/68"],
+            20: ["https://t.me/dragonrisingsubindo/69"],
+        }
+    }
+};
+
+// LOGIKA APLIKASI
+const contentArea = document.getElementById('contentArea');
+const searchInput = document.getElementById('searchInput');
+const logo = document.querySelector('.logo');
+
+// Render Halaman Utama (Grid Season)
+function renderHome(filter = "") {
+    contentArea.innerHTML = "";
+    const grid = document.createElement('div');
+    grid.className = 'grid-container';
+
+    Object.entries(VIDEO_DATABASE).forEach(([key, data]) => {
+        if (data.title.toLowerCase().includes(filter.toLowerCase())) {
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.onclick = () => renderDetail(key);
+
+            card.innerHTML = `
+                <div class="card-content">
+                    <div class="card-title">${data.title}</div>
+                    <div class="card-type">${data.type === 'season' ? 'TV Series' : 'Playlist'}</div>
+                </div>
+            `;
+            grid.appendChild(card);
+        }
+    });
+
+    if (grid.children.length === 0) {
+        contentArea.innerHTML = "<p style='text-align:center; color:#aaa;'>Tidak ditemukan.</p>";
+    } else {
+        contentArea.appendChild(grid);
+    }
+}
+
+// Render Halaman Detail (Episode List)
+function renderDetail(key) {
+    const data = VIDEO_DATABASE[key];
+    contentArea.innerHTML = "";
+
+    const container = document.createElement('div');
+    container.className = 'detail-view';
+
+    // Tombol Kembali
+    const backBtn = document.createElement('button');
+    backBtn.className = 'back-btn';
+    backBtn.innerHTML = "⬅ Kembali";
+    backBtn.onclick = () => renderHome();
+    container.appendChild(backBtn);
+
+    // Header Season
+    const header = document.createElement('div');
+    header.className = 'season-header';
+    header.innerHTML = `<h2>${data.title}</h2>`;
+    container.appendChild(header);
+
+    // List Episode
+    const list = document.createElement('div');
+    list.className = 'episode-list';
+
+    if (data.type === 'playlist') {
+        list.innerHTML = `
+            <a href="${data.url}" target="_blank" class="episode-item">
+                <span>📺 Tonton Full Playlist di YouTube</span>
+                <span class="watch-btn">Buka</span>
+            </a>
+        `;
+    } else if (data.type === 'playlist_multi') {
+        data.playlists.forEach(pl => {
+            list.innerHTML += `
+                <a href="${pl.url}" target="_blank" class="episode-item">
+                    <span>${pl.name}</span>
+                    <span class="watch-btn">Buka</span>
+                </a>
+            `;
+        });
+    } else {
+        // Tipe Season / Pilot (Episode List)
+        Object.entries(data.episodes).forEach(([epNum, links]) => {
+            const link = links[0]; // Ambil link pertama
+            const item = document.createElement('a');
+            item.className = 'episode-item';
+            item.href = link;
+            item.target = "_blank"; // Buka di tab baru (Telegram)
+
+            item.innerHTML = `
+                <span>Episode <span class="episode-number">${epNum}</span></span>
+                <span class="watch-btn">▶ Tonton</span>
+            `;
+            list.appendChild(item);
+        });
+    }
+
+    container.appendChild(list);
+    contentArea.appendChild(container);
+}
+
+// Event Listeners
+searchInput.addEventListener('input', (e) => {
+    renderHome(e.target.value);
+});
+
+logo.addEventListener('click', () => {
+    searchInput.value = "";
+    renderHome();
+});
+
+// Init
+renderHome();
